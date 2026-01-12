33c1be5006744d18eef2b1e23e576526858a79c5
DVectorprime <vectorprime2024@gmail.com>
2026-01-12 17:02:23 +0100

    codespaces reform

--- codeforces/Elephant.py
+++ codeforces/Elephant.py
@@ -1,8 +1,5 @@
-def findMinStep(coordinate_distance):
-    if coordinate_distance % 5 == 0:
-        return coordinate_distance // 5 
-    else:
-        return (coordinate_distance // 5) + 1
-    
-if __name__ == "__main__":
-    print(findMinStep(int(input("Enter distance from x = 0: "))))
+distance = int(input())
+
+steps = (distance + 4 ) // 5
+
+print(steps)
